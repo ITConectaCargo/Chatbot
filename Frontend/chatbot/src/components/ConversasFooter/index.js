6227cea27ca4935bd489a3f1e2a0a9b2027cd06f
wesleymoraesconecta <131008751+wesleymoraesconecta@users.noreply.github.com>
2023-04-19 17:31:11 -0300

    Conversas Footer

--- Frontend/chatbot/src/components/ConversasFooter/index.js
+++ Frontend/chatbot/src/components/ConversasFooter/index.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import { useState } from 'react'
+import styles from "./ConversasFooter.module.css"
+import { MdSend } from 'react-icons/md';
+import { IoMdHappy } from 'react-icons/io';
 
 export default function ConversasFooter() {
+  const [mensagem, setMensagem] = useState("");
+
   return (
-    <div>ConversasFooter</div>
+    <div className={styles.container}>
+      <form>
+      <IoMdHappy />
+        <input placeholder="Mensagem"
+          onChange={(e) => setMensagem(e.target.value)}
+          value={mensagem}/>
+          <MdSend />
+      </form>
+    </div>
   )
 }
